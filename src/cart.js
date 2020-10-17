66f7c41899b81e262375041d56d67ed63076a891
rakeshtt <rakeshyasht92@gmail.com>
2020-10-17 22:24:19 +0530

    first commit

---
+++ src/cart.js
@@ -0,0 +1,139 @@
+import allProducts from './input.json';
+
+export const cart = () => ({
+    details : {
+        total : 0,
+        discount : 0,
+        grandTotal : 0
+    },
+    items : {},
+    products : {},
+    init : function(){
+        for(var i=0;i<=allProducts.length;i++){
+            if(allProducts[i] !== undefined)
+                this.products[allProducts[i].name] = allProducts[i];
+
+            console.log(this.products)
+        }
+        this.list();
+    },
+    save : function () {
+        const { products, items } = this;
+        
+        const sumTotal = Object.keys(this.items).reduce((sum, item) => sum + products[item].price.actual * items[item], 0)
+        const discount = Object.keys(this.items).reduce((sum, item) => sum + ((products[item].price.actual/100) * products[item].discount)  * items[item], 0)
+        let grandTotal = sumTotal - discount;
+        grandTotal = grandTotal < 0 ? 0 : grandTotal;
+        this.details = {
+            total : sumTotal,
+            discount : discount,
+            grandTotal : grandTotal
+        }
+        
+        localStorage.setItem("cart", JSON.stringify({ details : this.details, items : this.items}));
+    },
+    load : function () {
+        this.items = localStorage.getItem("cart");
+        if (this.items == null) { this.items = {}; 
+        this.details = {
+            total : 0,
+            discount : 0,
+            grandTotal : 0
+        }}
+        else { 
+            const all = JSON.parse(this.items); 
+            this.items = all.items;
+            this.details = all.details;
+        }
+    },
+    list : function () {
+        const { products, items, details } = this;
+        const tblcart = document.querySelector('.tbl-cart tbody')
+        const totlcart = document.querySelector('.totl-cart')
+       
+        tblcart.querySelectorAll('*').forEach(n => n.remove())
+        if(Object.keys(this.items).length > 0)
+            Object.keys(this.items).forEach(function(item){
+                var template = '<tr>'+
+                '<td valign="middle">'+
+                    '<a href="#">'+
+                        '<img src="'+  products[item].image + '" title="" width="47" height="47">'+
+                        products[item].name +
+                    '</a>'+
+                '</td>'+
+                '<td>'+
+                '<div class="input-group">'+
+                    '<button data-pid="'+ products[item].name +'" data-value="0" class="addtocart input-group-btn">-</button>'+
+                    '<input type="text" name="" value="'+  items[item] +'" />'+
+                    '<button data-pid="'+  products[item].name +'" data-value="1" class="addtocart input-group-btn">+</button>'+
+                '</div>'+
+                '</td>'+
+                '<td class="price">$' + products[item].price.display + '</td>'+
+                '</tr>';
+                tblcart.insertAdjacentHTML('beforeend',template);
+            });
+        else {
+            var template = '<tr><td colspan="3">no items in cart<td></tr>';
+            tblcart.insertAdjacentHTML('beforeend',template);
+        }
+
+        var templatetl = 
+        '<tr>'+
+        '<tr>'+
+          '<td colspan="2">Total</td>'+
+        '</tr>'+
+        '<tr>'+
+          '<td>Items('+ Object.keys(this.items).length +')</td>'+
+          '<td class="price"> $'+ details.total +'</td>'+
+        '</tr>'+
+        '<tr>'+
+          '<td>Discount</td>'+
+          '<td class="price"> $'+ details.discount +'</td>'+
+        '</tr>'+
+        '<tfoot>'+
+          '<tr>'+
+            '<td>Grand Total</td>'+
+            '<td class="price"> $'+ details.grandTotal +'</td>'+
+          '</tr>'+
+      '</tfoot>'+
+      '</tr>';
+      totlcart.querySelectorAll('*').forEach(n => n.remove())
+      totlcart.insertAdjacentHTML('beforeend',templatetl);
+       
+    },
+    change : function(){
+         
+      this.save();
+      this.load();
+      this.list();
+    },
+    add : function (name) {
+       
+      if (this.items[name] == undefined) {
+        this.items[name] = 1;
+      } else {
+        this.items[name]++;
+      }
+
+      this.change();
+     
+    },
+    sub : function (name) {
+       
+        if (this.items[name] == undefined) {
+          this.items[name] = 1;
+        } else {
+          this.items[name]--;
+        }
+
+        if(this.items[name] <= 0)
+            this.remove(name);
+        else
+            this.change();
+     
+    },
+    remove : function (name) {
+      delete this.items[name];
+      this.change();
+    }
+});
--- src/index.js
+++ src/index.js
@@ -1,14 +1,57 @@
 import './main.css';
-import styles from './index.module.css';
+import styles from './custom.module.sass';
+import products from './input.json';
+import { cart } from './cart.js';
+const cartItems = new cart();
+cartItems.init();
+const prodbox = document.querySelector('#prod-box');
 
-const h1 = document.querySelector('h1');
-h1.classList.add(styles.h1);
-
-async function print() {
-  // Here we are using dynamic import
-  const { greet } = await import('./greet');
-  const response = await greet('John Doe');
-  console.log(response);
+const addProduct = (product) => {
+  alert(product);
 }
+let messagetimer;
+const popmessage = document.querySelector('.pop-message');
+document.addEventListener('click', function (event) {
+	if (event.target.matches('.addtocart')) {
+    if(Boolean(Number(event.target.dataset.value))){
+      popmessage.style.display = "block";
+      clearTimeout(messagetimer);
+      messagetimer = setTimeout(function(){
+        popmessage.style.display = "none";
+        
+      }, 3000)
+      popmessage.innerHTML = event.target.dataset.pid + " added to cart";
+      cartItems.add(event.target.dataset.pid)
+    } else {
+      cartItems.sub(event.target.dataset.pid)
+    }
+	}
+}, false);
 
-print();
+products.forEach(function(product){
+     var template = ' <div class="column">' +
+                        '<div class="prod-container">' +
+                          '<div class="prod-body">'+
+                            '<div class="off-lable">$'+ product.discount +'%</div>'+
+                            '<img src="'+  product.image +'" alt="image">'+ 
+                          '</div>'+
+                          '<div class="prod-footer">'+
+                            '<div class="left-item">'+
+                              '<div class="product-title">'+
+                                product.name +
+                              '</div>'+
+                              '<div class="product-price">'+
+                                  '<span class="strike-price">$' + product.price.display +  ' </span> &nbsp;'+  
+                                  '<span class="off-price">$'+  product.price.actual + '</span>'+
+                              '</div>'+
+                            '</div>'+
+                            '<div class="right-item">'+
+                              '<button class="addtocart" data-value="1" data-pid="' + product.name + '">Add to cart</button>'+
+                            '</div>'+
+                          '</div>'+
+                        '</div>'+
+                      '</div>'+
+                    '</div>';
+        prodbox.insertAdjacentHTML('beforeend',template);
+});
+       
